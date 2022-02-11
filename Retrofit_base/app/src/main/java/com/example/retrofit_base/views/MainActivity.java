e9be6ff2a622d56ed83f5702bdae1fe7b857c238
halHalberstadt <90930042+halHalberstadt@users.noreply.github.com>
2022-02-11 12:22:15 -0800

    Uploading Files for Repo

---
+++ Retrofit_base/app/src/main/java/com/example/retrofit_base/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.retrofit_base.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.retrofit_base.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
